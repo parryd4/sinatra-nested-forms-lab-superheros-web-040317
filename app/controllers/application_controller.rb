57279d0d4ad8fc7fbc3d5ec37c87f899114c99ef
Dan Parry <parryd424@gmail.com>
2017-04-26 14:32:51 -0400

    syntax fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,16 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :submission_form
+    end
+
+    post '/teams' do
+  
+      @team = params["team"]
+
+      erb :team
+
+    end
 
 end
